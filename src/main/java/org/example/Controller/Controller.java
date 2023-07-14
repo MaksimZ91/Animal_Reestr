fa5e67925b4b26876f2c4b218aab3511cc757a41
MaksimZ91 <ak4202@mail.ru>
2023-07-14 14:00:51 +0800

    ui in progress

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -1,12 +1,19 @@
 package org.example.Controller;
 
+import org.example.Animals.Registry;
 import org.example.UI.UI;
 
 public class Controller {
     public void start(){
         UI mmenu = new UI();
-        switch (mmenu.mainMenu())
-            case 1;
+        Registry rg = new Registry();
+        switch (mmenu.mainMenu()) {
+            case 1 -> System.out.println("заглушка");
+            case 2 -> System.out.println(rg.getHomeAnimalList());
+            case 3 -> System.out.println(rg.getPackAnimalList());
+            default -> {return;}
+        }
+
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,15 @@
 package org.example;
 
 import org.example.Animals.*;
+import org.example.Controller.Controller;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        Registry rg = new Registry();
-        Cats ct = new Cats("me", "11.08.2022");
-        Horse hr = new Horse("uj", "11.08.2021");
-
-        rg.add(ct);
-        ct.educateCommand("ktf0", "ffg");
-
-
-        rg.add(hr);
-        System.out.println(rg.getHomeAnimalList());
-        System.out.println(rg.getPackAnimalList());
-        System.out.println(rg.viewCommandAnimals(ct));
+        Controller ct = new Controller();
+        ct.start();
 
 
 
